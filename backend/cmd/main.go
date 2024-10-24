7942550d628e2549b90f3789828c6fd7ed8492b8
Ayush5036 <guptaayushcv45@gmail.com>
2024-10-24 21:27:28 +0530

    commit

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -57,11 +57,11 @@ var store = &RuleStore{
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	// Use `mongo.Connect` directly to connect to MongoDB
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
